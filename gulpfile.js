5243d9c2703e703d9364b722a1641b74d4e3209e
Hardi Suputra <cokadie@gmail.com>
2018-03-01 09:08:05 +0800

    Update Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,37 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 
+var browserSync = require( 'browser-sync' ).create();
+var browserReload = browserSync.reload;
+
 var styleSRC = 'src/scss/style.scss';
 var styleDIST = './public_html/dist/css/';
-var styleWatch = 'src/scss/**/*.scss'
+var styleWatch = 'src/scss/**/*.scss';
 
 var jsSRC = 'script.js';
 var jsFolder = 'src/js/';
 var jsDIST = './public_html/dist/js/';
 var jsWatch = 'src/js/**/*.js';
-var jsFiles = [jsSRC,]
+var jsFiles = [jsSRC];
+
+var htmlWatch = '**/*.html';
+var phpWatch = '**/*.php';
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        open: false,
+        injectChanges: true,
+        proxy: 'http://gulpdev.dd'
+        /* if need https */
+        // https: {
+        //      key: 'path/to/[file].key'
+        //      cert: 'path/to/[cert].key
+        // }
+        // server: {
+        //      baseDir: './public_html'
+        // }
+    });
+});
 
 
 gulp.task('style', function(){
@@ -36,6 +58,7 @@ gulp.task('style', function(){
         .pipe( rename( { suffix: '.min' } ) )
         .pipe( sourcemaps.write('./') )
         .pipe( gulp.dest( styleDIST ) )
+        .pipe( browserSync.stream() )
     ;
 });
 
@@ -59,16 +82,16 @@ gulp.task('js', function(){
         .pipe( uglify() )
         .pipe( sourcemaps.write('./'))
         .pipe( gulp.dest( jsDIST ))
+        .pipe( browserSync.stream() )
     });
     
 });
 
-gulp.task('default', ['style', 'js'], function(){
-    //TO DO
-});
+gulp.task('default', ['style', 'js']);
 
-gulp.task('watch', ['default'], function(){
+gulp.task('watch', ['default', 'browser-sync'], function(){
    gulp.watch( styleWatch, ['style'] );
-   gulp.watch( jsWatch, ['js'] ); 
+   gulp.watch( jsWatch, ['js', browserReload] ); 
+   gulp.watch( htmlWatch, browserReload);
 
 });
